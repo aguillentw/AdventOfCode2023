ffbcc7e288d36a3b91bc633cc5b1e6f034ac4a89
Abel Guillen <abel.guillenfuerte@thoughtworks.com>
2023-12-03 23:56:20 +0100

    feat(day01): Solution to the first problem

--- src/day01.rs
+++ src/day01.rs
@@ -1,12 +1,47 @@
-pub fn exercice(content: String) -> i32 {
-    return content.len() as i32;
+pub fn exercice1(content: String) -> u32 {
+    let mut result = 0;
+
+    let lines = content.split("\n");
+    for line in lines {
+        if line.is_empty() {
+            continue;
+        }
+        let calibrated_value = find_calibrated_value(line.to_string());
+        result += calibrated_value;
+    }
+
+    return result;
 }
 
+pub fn exercice2(_content: String) -> u32 {
+    let result = 0;
+    return result;
+}
+
+fn find_first_digit(content: String) -> char {
+    let result = content.find(|c: char| c.is_ascii_digit()).unwrap();
+    return content.chars().nth(result).unwrap();
+}
+
+fn find_last_digit(content: String) -> char {
+    let result = content.rfind(|c: char| c.is_ascii_digit()).unwrap();
+    return content.chars().nth(result).unwrap();
+}
+
+fn find_calibrated_value(content: String) -> u32 {
+    let first_digit = find_first_digit(content.clone());
+    let last_digit = find_last_digit(content.clone());
+
+    let first_digit_value = first_digit.to_digit(10).unwrap();
+    let last_digit_value = last_digit.to_digit(10).unwrap();
+
+    return (first_digit_value * 10) + last_digit_value;
+}
 
 
 #[cfg(test)]
 mod tests {
-    use crate::day01::exercice;
+    use super::*;
 
     #[test]
     fn it_works() {
@@ -22,6 +57,42 @@ a1b2c3d4e5f
 treb7uchet");
 
         // When/Then
-        assert_eq!(exercice(test_content), 40);
+        assert_eq!(exercice1(test_content), 142);
+    }
+
+    #[test]
+    fn should_find_the_first_digit_in_a_string() {
+        // Given
+        let test_content = String::from("asdfa91abc2");
+
+        // When
+        let result = find_first_digit(test_content);
+
+        // Then
+        assert_eq!(result, '9');
+    }
+
+    #[test]
+    fn should_find_the_last_digit_in_a_string() {
+        // Given
+        let test_content = String::from("asdfa91abc2");
+
+        // When
+        let result = find_last_digit(test_content);
+
+        // Then
+        assert_eq!(result, '2');
+    }
+
+    #[test]
+    fn should_find_the_calibrated_value() {
+        // Given
+        let test_content = String::from("asdfa91abc2assdf");
+
+        // When
+        let result = find_calibrated_value(test_content);
+
+        // Then
+        assert_eq!(result, 92);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@ use std::fs;
 pub mod day01;
 
 fn main() {
-    println!("Starting Advent of Code 2023!");
+    println!("");
+    println!("❄️  Starting Advent of Code 2023 ❄️");
+    println!("");
 
-    println!("Day 01");
-    let file_path = "resources/day01.txt";
+    println!("========= Day 01 =========");
+    let file_path = "inputs/day01_1.txt";
     let contents = fs::read_to_string(file_path)
         .expect("Path not readable");
-
-    day01::exercice(contents);
+    println!("Ex.1 - The sum of all calibration values is {}", day01::exercice1(contents));
+    let file_path = "inputs/day01_2.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("Path not readable");
+    println!("Ex.2 - The sum of all calibration values is {}", day01::exercice2(contents));
 }
